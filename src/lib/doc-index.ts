ca63d0cee798232dc98d5f65cc64b52d91feadca
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2025-12-02 14:26:23 +0800

    refactor(DocLink): allow absolute path and src

--- src/lib/doc-index.ts
+++ src/lib/doc-index.ts
@@ -57,8 +57,8 @@ async function getOrBuildKeyIndex(): Promise<Map<string, string>> {
  */
 export async function getLinkToKey(key: string): Promise<string | undefined> {
   const index = await getOrBuildKeyIndex();
-  if (key.startsWith("/")) {
-    if (Object.values(index).some(slug => `/${slug}/` === key || `/${slug}` === key))
+  if (key?.startsWith("/")) {
+    if (index.values().some(slug => `/${slug}/` === key || `/${slug}` === key))
       return key;
     else
       return undefined;
