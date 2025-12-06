1732a257f3fa5cd11d926c59f98127d84b6c0986
Sudrut <sudrut2025@outlook.com>
2025-12-06 11:58:22 +0800

    style: reformat code

--- src/plugins/expressive-code/cxx-mark.ts
+++ src/plugins/expressive-code/cxx-mark.ts
@@ -1,4 +1,8 @@
-import { definePlugin, InlineStyleAnnotation, type ExpressiveCodePlugin } from "@astrojs/starlight/expressive-code";
+import {
+  definePlugin,
+  InlineStyleAnnotation,
+  type ExpressiveCodePlugin,
+} from "@astrojs/starlight/expressive-code";
 
 export function pluginCxxMark(): ExpressiveCodePlugin {
   return definePlugin({
@@ -7,51 +11,55 @@ export function pluginCxxMark(): ExpressiveCodePlugin {
       postprocessAnalyzedCode: (context) => {
         context.codeBlock.getLines().forEach((line) => {
           if (context.codeBlock.meta.includes("cxx-mark")) {
-            const matches = [...line.text.matchAll(/\/\*\$(((?<syntax>s)|(?<exposition>expos)):[^\*]+|(?<optional>opt))\*\//g)].reverse()
+            const matches = [
+              ...line.text.matchAll(
+                /\/\*\$(((?<syntax>s)|(?<exposition>expos)):[^\*]+|(?<optional>opt))\*\//g
+              ),
+            ].reverse();
             matches.forEach((match) => {
-              const begin = match.index
-              const end = begin + match[0].length
+              const begin = match.index;
+              const end = begin + match[0].length;
               if (match.groups?.syntax != undefined) {
                 line.addAnnotation(
                   new InlineStyleAnnotation({
                     inlineRange: {
                       columnStart: begin,
-                      columnEnd: end
+                      columnEnd: end,
                     },
                     // color of syntax notation should be same with comments
-                    italic: true
+                    italic: true,
                   })
-                )
-                line.editText(begin, end, match[0].slice(5, -2))
+                );
+                line.editText(begin, end, match[0].slice(5, -2));
               } else if (match.groups?.exposition != undefined) {
                 line.addAnnotation(
                   new InlineStyleAnnotation({
                     inlineRange: {
                       columnStart: begin,
-                      columnEnd: end
+                      columnEnd: end,
                     },
                     color: "var(--cppdoc-color-cxx-mark-exposition)",
-                    italic: true
+                    italic: true,
                   })
-                )
-                line.editText(begin + 2, begin + 9, "")
+                );
+                line.editText(begin + 2, begin + 9, "");
               } else if (match.groups?.optional != undefined) {
-                const new_str = "(optional)"
-                line.editText(begin, end, new_str)
+                const new_str = "(optional)";
+                line.editText(begin, end, new_str);
                 line.addAnnotation(
                   new InlineStyleAnnotation({
                     inlineRange: {
                       columnStart: begin,
-                      columnEnd: begin + new_str.length
+                      columnEnd: begin + new_str.length,
                     },
-                    color: "var(--cppdoc-color-cxx-mark-optional)"
+                    color: "var(--cppdoc-color-cxx-mark-optional)",
                   })
-                )
+                );
               }
-            })
+            });
           }
-        })
-      }
-    }
-  })
+        });
+      },
+    },
+  });
 }
