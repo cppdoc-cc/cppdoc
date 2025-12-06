7d24ede670c991f17cf93c154b28d6655ef44d29
Sudrut <sudrut2025@outlook.com>
2025-12-06 12:14:56 +0800

    style: reformat code

--- ec.config.mjs
+++ ec.config.mjs
@@ -4,4 +4,4 @@ import { pluginCxxMark } from "./src/plugins/expressive-code/cxx-mark.ts";
 /** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
 export default {
   plugins: [pluginCxxMark()],
-}
+};
